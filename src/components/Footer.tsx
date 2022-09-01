b221443485a705635227b477cc20f81ac9ae30e7
viniciusIrikawa <viniciusiandrade@hotmail.com>
2022-09-01 16:57:09 -0300

    Responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,31 +3,31 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-zinc-800 py-5 w-full py-7 px-12'>
-      <div className='md:flex justify-around'>
-        <div className='text-[#fff] text-4xl font-serif'> Lexir </div>
+      <div className='md:flex md:justify-around'>
+        <div className='text-[#fff] text-4xl font-serif md:text-left text-center'> Lexir </div>
 
-        <ul className='text-[#fff]'>
+        <ul className='text-[#fff] md:text-left text-center md:my-0 my-8'>
           <span className='uppercase text-[#8F8F8F] '> Community</span>
           <li className='my-2'>Lexir for Brands</li>
           <li className='my-2'>Business Buyers </li>
           <li className='my-2'>Sales Affiliates </li>
         </ul>
 
-        <ul className='text-[#fff]'>
+        <ul className='text-[#fff] md:text-left text-center md:my-0 my-8'>
           <span className='uppercase text-[#8F8F8F]'> Platform </span>
           <li className='my-2'>Resources</li>
           <li className='my-2'>Pricing </li>
           <li className='my-2'>Get Started </li>
         </ul>
 
-        <ul className='text-[#fff]'>
+        <ul className='text-[#fff] md:text-left text-center md:my-0 my-8'>
           <span className='uppercase text-[#8F8F8F]'> Company</span>
           <li className='my-2'>About</li>
           <li className='my-2'>Contact </li>
           <li className='my-2'>Legal </li>
         </ul>
 
-        <ul className='text-[#fff]'>
+        <ul className='text-[#fff] md:text-left text-center md:my-0 my-8'>
           <span className='uppercase text-[#8F8F8F]'> Lexir Shop</span>
           <li className='my-2'>Brands</li>
           <li className='my-2'>Spirits </li>
