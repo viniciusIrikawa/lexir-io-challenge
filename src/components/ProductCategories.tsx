bc772e4b62cb66d30c331834fdd25e33cc933f80
viniciusIrikawa <viniciusiandrade@hotmail.com>
2022-09-05 16:09:35 -0300

    Refactoring Category buttons

--- src/components/ProductCategories.tsx
+++ src/components/ProductCategories.tsx
@@ -1,19 +1,7 @@
 import React, { useContext } from 'react'
 import { products } from 'src/pages/api/products';
 import {ProductContext} from '../Context/ContextProducts'
-
-// interface Category {
-//     categoryName: string[];
-// }
-
-// let productCategory: object;
-
-// productCategory = {
-//     categoryName: ['Gin', 'Vodka']
-// }
-
-// const categories:any = Object.values(productCategory)
-// console.log(categories)
+import {categories} from '../pages/api//categories'
 
 const ProductCategories = () => {
   const {listProducts, setListProducts} = useContext(ProductContext);
@@ -35,16 +23,20 @@ const ProductCategories = () => {
         <h4 className='mb-2 text-lg'> Product Categories </h4>
       </div>
 
-      <div> 
-        <button type="button" 
-                onClick={() => setListProducts(products)} 
-                className="bg-emerald-400 py-1 px-5 m-1 rounded bg-gray-100"> All </button>
-        <button type="button" 
-                onClick={() => filterCategory('Gin')} 
-                className="bg-emerald-400 py-1 px-5 m-1 rounded bg-gray-100" > Gin </button>
-        <button type="button" 
-                onClick={() => filterCategory('Vodka')} 
-                className="bg-emerald-400 py-1 px-5 m-1 rounded bg-gray-100" > Vodka </button>
+      <div>
+        {categories.map((item, index) => item === 'All' ? 
+        (
+          <button type="button"
+                  key={index} 
+                  onClick={() => setListProducts(products)} 
+                  className="bg-emerald-400 py-1 px-5 m-1 rounded bg-gray-100"> All </button>
+        ) : (
+          <button type="button"
+                  key={index} 
+                  onClick={() => filterCategory(`${item}`)} 
+                  className="bg-emerald-400 py-1 px-5 m-1 rounded bg-gray-100" > {item} </button>
+        ))}
+
       </div>
     </div>
   )
