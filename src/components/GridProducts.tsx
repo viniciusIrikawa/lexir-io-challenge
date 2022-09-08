967a26b85206da8f04122a2d029466e759f3cdf3
Vinicius <viniciusiandrade@hotmail.com>
2022-09-07 21:55:22 -0300

    Visual styles

--- src/components/GridProducts.tsx
+++ src/components/GridProducts.tsx
@@ -13,9 +13,13 @@ const GridProducts = () => {
             <div className='imgWrapper bg-[#F2F2F2] w-full py-5 flex justify-center items-center'> 
               <Image src={item.image} width={60} height={190} alt={item.name}></Image>
             </div>
-            <div className='contentWrapper w-full p-3 shadow-lg flex justify-between items-center'> 
-              <strong> <h3 className='itemName text-[#2D2D2D] uppercase'> {item.name} </h3> </strong>
-              <span className='price text-[#6B6B6B]'> €{item.price} </span> 
+            <div className='contentWrapper w-full p-3 shadow-lg'> 
+              <div className='w-full flex justify-between items-center pt-2 pb-3'>
+                <strong> <h3 className='itemName text-[#2D2D2D] uppercase'> {item.name} </h3> </strong>
+                <span className='price text-[#6B6B6B]'> €{item.price} </span>
+              </div>
+              <button className='py-1 px-10 mt-2 w-full bg-[#4be64bc9] rounded-md'> Add to cart </button>
+            
             </div>
           </li>
         ))}
