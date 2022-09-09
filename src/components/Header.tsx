2c3a278faacfec636531d35449b2e1ad92c209fa
Vinicius <viniciusiandrade@hotmail.com>
2022-09-08 23:21:31 -0300

    Adjustment - Item counter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                         </svg>
-                        <span className='counterItems bg-[#ffaca7] absolute -top-1 -right-1 px-1 rounded-full'> {cartItems.length} </span>
+                        <span className='counterItems bg-red-500 absolute -top-0.5 -right-1 rounded-full w-6 text-center text-[#fff] font-bold'> {cartItems.length} </span>
                     </div>
 
                     <Link href={''}>
